60425d32c837af2d36c008f0ec20b849569420c9
Ecsedi Tamás József <ecsi.hgh@gmail.com>
2023-12-31 18:37:34 +0100

    ninja mozgás

--- main.py
+++ main.py
@@ -7,11 +7,25 @@ HEIGHT=620
 BG_COLOR=(255,255,255)
 FPS=60
 
-class Ninja(pygame.sprite.Sprite): #sprite osztály
+class Ninja(pygame.sprite.Sprite): #ninja osztály
     def __init__(self):
         super().__init__()
         self.image=pygame.image.load('img/Idle__000 1.png').convert_alpha()
         self.rect=self.image.get_rect(midbottom=(WIDTH/2,HEIGHT-100))
+        self.speed=5
+
+    def ninja_input(self):
+        keys=pygame.key.get_pressed() #ninja mozgása
+        if keys[pygame.K_RIGHT]and self.rect.right<WIDTH:
+            self.x_movement(self.speed)
+        if keys[pygame.K_LEFT] and self.rect.left>0:
+            self.x_movement(-self.speed)
+
+    def x_movement(self,dx): #dx lesz a self.speed
+        self.rect.x+=dx
+    
+    def update(self):
+        self.ninja_input()
 
 pygame.init() #inicializálja magát a pygame
 screen=pygame.display.set_mode((WIDTH,HEIGHT)) #meghatározza az ablakot
