d801f4acb6d913b57ab2c7a78a44fa1b20aead88
Ecsedi Tamás József <ecsi.hgh@gmail.com>
2025-02-04 21:53:18 +0100

    HEJ update

--- main.py
+++ main.py
@@ -1,19 +1,49 @@
-import pygame
-import random
+import pygame, random
 
-#állandó értékek
-WIDTH=1280
-HEIGHT=620
 BG_COLOR=(255,255,255)
-FONT_COLOR=(27,131,142)
+FONT_COLOR=(255,255,255)
 FPS=60
 
-#háttérkép beállítása
+pygame.init()
+pygame.mixer.init()
+
+#ÁLLANDÓ ÉRTÉKEK
+
+info = pygame.display.Info()
+WIDTH, HEIGHT = info.current_w, info.current_h
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
+pygame.display.set_caption('Fruit Ninja')
+clock=pygame.time.Clock() #időzítő
+GAME_TIME=61000 #játékidő
+start_time=pygame.time.get_ticks() #visszaszámlálás
+game_font=pygame.font.SysFont('arial',30,bold=True) #betűtípus
+
+#háttérkép 
 background = pygame.image.load('img/background.jpg')
 background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 background.set_alpha(200) #háttér legyen halványabb
 
-class Ninja(pygame.sprite.Sprite): #ninja osztály
+#vágás anime
+slash_surf=pygame.image.load('img/slash.png').convert_alpha()
+slash_surf=pygame.transform.scale(slash_surf,(WIDTH,HEIGHT))
+slash_rect=slash_surf.get_rect(topleft=(0,0))
+
+#hangok
+pygame.mixer.set_num_channels(3)
+channel1 = pygame.mixer.Channel(0)
+channel2 = pygame.mixer.Channel(1)
+channel3 = pygame.mixer.Channel(2)
+theme_song = pygame.mixer.Sound('sound/theme.mp3')
+slash_sound = pygame.mixer.Sound('sound/slash.mp3')
+jump_sound = pygame.mixer.Sound('sound/jump2.mp3')
+theme_song.set_volume(0.3)
+jump_sound.set_volume(2)
+slash_sound.set_volume(2)
+channel1.play(theme_song, loops=-1)
+
+#ÁLLANDÓ ÉRTÉKEK VÉGE#
+
+class Ninja(pygame.sprite.Sprite): #játékos
     def __init__(self):
         super().__init__()
 
@@ -32,8 +62,8 @@ class Ninja(pygame.sprite.Sprite): #ninja osztály
         #támadások animálása jobbra
         NINJA_ATTACK_1=pygame.image.load('img/Jump_Attack__002.png').convert_alpha()
         NINJA_ATTACK_2=pygame.image.load('img/Attack__009.png').convert_alpha()
-        NINJA_ATTACK_3=pygame.image.load('img/Attack__009.png').convert_alpha()
-        NINJA_ATTACK_4=pygame.image.load('img/Jump_Attack__007.png').convert_alpha()
+        NINJA_ATTACK_3=pygame.image.load('img/Jump_Attack__007.png').convert_alpha()
+        NINJA_ATTACK_4=pygame.image.load('img/Attack__009.png').convert_alpha()
         self.ninja_attack_fw=[NINJA_ATTACK_1,NINJA_ATTACK_2,NINJA_ATTACK_3,NINJA_ATTACK_4]
 
         #támadás balra
@@ -43,41 +73,53 @@ class Ninja(pygame.sprite.Sprite): #ninja osztály
         NINJA_ATTACK_4b=pygame.transform.flip(NINJA_ATTACK_4,True,False)
         self.ninja_attack_bw=[NINJA_ATTACK_1b,NINJA_ATTACK_2b,NINJA_ATTACK_3b,NINJA_ATTACK_4b]
 
-        self.ninja_index=0 #melyik kép a listából
+        self.ninja_index=0 
         self.ninja_attack=self.ninja_attack_fw
 
-        self.image_original=pygame.image.load('img/Idle__000 1.png').convert_alpha() #jobbra néző állókép
-        self.image_flipped=pygame.transform.flip(self.image_original,True,False) #balra néző állókép
+        #idle
+        self.image_original=pygame.image.load('img/Idle__000 1.png').convert_alpha() #jobbra 
+        self.image_flipped=pygame.transform.flip(self.image_original,True,False) #balra 
         
-        self.image=self.ninja_fw[self.ninja_index] #maga az adott kép
-        self.rect=self.image.get_rect(midbottom=(WIDTH/2,HEIGHT-100)) #képernyő aljára lett rakva
+        #player surf/rect
+        self.image=self.ninja_fw[self.ninja_index] 
+        self.rect=self.image.get_rect(midbottom=(WIDTH/2,100))
+        
+        #RECT MÓDOSÍTÁSOK - sajnos nem működik 
+        #self.rect.inflate_ip(-50, 0) #csökkenti a rect szélességét
+        #self.rect.width *= 0.7 #csökkenti ez is a rect szélességét
+        #self.rect.inflate_ip(-self.rect.width * 0.3, 0)  # Csökkenti a szélességet 30%-kal
+        #self.rect = pygame.Rect(self.rect.x, self.rect.y, int(self.rect.width * 0.5), self.rect.height)
 
-        self.ninja_speed=8 #mozgási sebesség
-        self.ninja_forward=True #melyik irányba megy
-        self.attack_mode=False #támad-e?
+        #mozgás
+        self.ninja_speed=12 
+        self.ninja_forward=True 
+        self.attack_mode=False 
         
         #UGRÁS ELEMEI
         self.on_ground=True
         self.gravity=1
-        self.jump_speed=-18
+        self.jump_speed=-25
         self.dy=0
+        #self.jump_twice=0 #dupla ugrás, sajnos nem működik
 
+    #IRÁNYÍTÁS
     def ninja_input(self):
-        keys=pygame.key.get_pressed() #ninja mozgása
+        keys=pygame.key.get_pressed() 
 
-        if keys[pygame.K_LCTRL]: #támadás
+        if keys[pygame.K_LCTRL] and self.on_ground: #támadás
             self.attack_mode=True
             self.attack_animation()
         else:
-            self.attack_mode=False #ha nincs space nincs attack mód
+            self.attack_mode=False 
 
-        if keys[pygame.K_SPACE] and self.on_ground: #space ugrik ha a földön van
-            self.jump() #lásd lentebb ugrás fizika
+        if keys[pygame.K_SPACE] and self.on_ground: #ugrás
+            self.jump() 
+            channel3.play(jump_sound)
 
-        if keys[pygame.K_RIGHT] and self.rect.right<WIDTH: #jobbra
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]: #jobbra
             self.x_movement(self.ninja_speed)
             self.ninja_forward=True
-        if keys[pygame.K_LEFT] and self.rect.left>0: #ballra
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]: #balra
             self.x_movement(-self.ninja_speed)
             self.ninja_forward=False
 
@@ -90,43 +132,52 @@ class Ninja(pygame.sprite.Sprite): #ninja osztály
             self.on_ground=True
         '''
 
-        if not any(keys): #ha nincs lenyomott billentyű
-            if self.ninja_forward: #álló helyzetben milyen kép legyen
-                self.image=self.image_original #álló kép jobbra néz
+        if not any(keys): #idle
+            if self.ninja_forward: 
+                self.image=self.image_original 
             else:
-                self.image=self.image_flipped #állókép balra néz
-            rect_center=self.rect.center #lekérjük a rect adatait, mert álva kisebb
-            self.rect=self.image.get_rect(center=rect_center) #és felülírjuk a mozgós rect adatokat, mert így kisebb
+                self.image=self.image_flipped 
+            rect_center=self.rect.center 
+            self.rect=self.image.get_rect(center=rect_center) #
 
-    def attack_animation(self): #támadás animálásának indexei
+    def attack_animation(self): #támadás animálása
         if self.ninja_index<len(self.ninja_fw)-1:
             self.ninja_index+=0.1
         else:
             self.ninja_index=0
 
-        if self.ninja_forward: #jobbra kardozás
+        if self.ninja_forward: #jobbra 
             self.image=self.ninja_attack_fw[int(self.ninja_index)]
-        else: #balrakardozás
+        else: #balra
             self.image = self.ninja_attack_bw[int(self.ninja_index)]
 
-    #UGRÁS
-    def jump(self):
-        self.on_ground=False #nem vagyunk a földön
-        self.dy=self.jump_speed  #minusszal kezd, a hozzáadás miatt átmegy pluszba, mint egy görbe
+    def jump(self): #ugrás
+        self.on_ground=False 
+        self.dy=self.jump_speed
         
-    def apply_gravity(self): #ugrásban a süllyedés
+    def apply_gravity(self): #gravitáció
         self.dy+=self.gravity
-        self.rect.y+=self.dy  #gravitáció a négyzeten... majdnem (mindig ad hozzá)
+        if self.dy>35:
+            self.dy=35
+        self.rect.y+=self.dy 
 
     def y_movement_collision(self): #ugrás utáni érkezés a platformra
-        for pf_rect in platform_rects: #azért for mert több platform van
-            if self.rect.colliderect(pf_rect):  # Ütközés vizsgálata a platformmal
-                self.rect.bottom = pf_rect.top  # A ninja alja az elfogadható tartomány alsó határánál helyezkedik el
-                self.dy = 0  # Zuhanás sebessége nulla
-                self.on_ground = True  # Földön vagyunk
+        for pf_rect in platform_rects: 
+            if self.rect.colliderect(pf_rect):
+                if self.rect.bottom >= pf_rect.top and self.rect.bottom <= pf_rect.top + abs(self.dy):
+                    self.rect.bottom = pf_rect.top  
+                    self.dy = 0  
+                    self.on_ground = True  
+                    
+        if self.rect.colliderect(platformSMALL_rect): 
+            if self.rect.bottom >= platformSMALL_rect.top and self.rect.bottom <= platformSMALL_rect.top + abs(self.dy):
+                self.rect.bottom = platformSMALL_rect.top  
+                self.dy = 0  
+                self.on_ground = True
 
-    def x_movement(self,dx): #dx lesz a self.ninja_speed
-        self.rect.x+=dx #horizontális mozgás
+    #horizontális mozgás
+    def x_movement(self,dx): 
+        self.rect.x+=dx 
         self.x_movement_animation()
 
     def x_movement_animation(self): #mozgás animáció indexe
@@ -140,109 +191,205 @@ class Ninja(pygame.sprite.Sprite): #ninja osztály
         else:
             self.image=self.ninja_bw[int(self.ninja_index)] #ez a balra mozgás
 
+    def y_movement(self,dy): #vertikális mozgás
+        self.rect.y+=dy 
 
-    def y_movement(self,dy): #dy az a self.ninja_speed
-        self.rect.y+=dy #vertikális mozgás
+    def hopp(self): #végtelen képernyős mozgás
+        if self.rect.top > HEIGHT+50:
+            self.rect.bottom = -50
+        if self.rect.right < -50:
+            self.rect.left = WIDTH+50
+        if self.rect.left > WIDTH+50:
+            self.rect.right = -50
 
     def update(self):
-        self.ninja_input() #objektumon belül történik az elem frissítése
-        self.apply_gravity() #ugrás utáni zuhanás sebesség
-        self.y_movement_collision() #hova érjen vissza
+        self.ninja_input()
+        self.apply_gravity() 
+        self.y_movement_collision()
+        self.hopp()
 
 class Fruit(pygame.sprite.Sprite):
-    def __init__(self, fruit_type): #fruit type határozza meg mi fog leesni
+    def __init__(self, fruit_type): #fruit_type: körte, banán, eper
         super().__init__()
 
-        self.fruit_speed=5
+        self.fruit_speed=random.randint(5,8) #gyümölcs sebessége
 
         if fruit_type=='pear': #körte
-            self.image=pygame.image.load('img/pear.png').convert_alpha() #körte kép
+            self.image=pygame.image.load('img/pear.png').convert_alpha() 
         elif fruit_type=='banana': #banán
-            self.image=pygame.image.load('img/banana.png').convert_alpha() #banán kép
+            self.image=pygame.image.load('img/banana.png').convert_alpha() 
         else: #eper
-            self.image=pygame.image.load('img/strawberry.png').convert_alpha() #eper kép
+            self.image=pygame.image.load('img/strawberry.png').convert_alpha() 
 
-        self.rect=self.image.get_rect(center=(random.randint(20,WIDTH-20),-20)) #honnan induljon le, képernyő fölött kezd, véletlen a horizonton
+        self.rect=self.image.get_rect(center=(random.randint(20,WIDTH-20),-20)) #kiindulási hely
 
-    def fruit_movement(self,dy): #gyömölcs esésének metódusa
+    def fruit_movement(self,dy): #gyümölcs mozgása
         self.rect.y+=dy
 
-    def destroy(self):
+    def destroy(self): #törlés
         if self.rect.top>HEIGHT:
-            self.kill() #törli magát ha a teteje több mint a képernyő magassága
+            self.kill() 
 
-    def update(self): #gyümölcs magának a frissítése
-        self.fruit_movement(self.fruit_speed) #gyümölcs sebességével esik
+    def update(self): 
+        self.fruit_movement(self.fruit_speed) 
         self.destroy()
 
-def collision_sprite(): #ütközések vizsgálata
-    if ninja.sprite.attack_mode: #ha ninja támad
+def collision_sprite(): #ütközések 
+    if ninja.sprite.attack_mode: 
         if pygame.sprite.spritecollide(ninja.sprite, fruit_group, True): #és ha találkozik egy gyümölcsel
+            slash_anime(slash_surf,slash_rect)
+            channel2.play(slash_sound)
             return True
     return False
 
+def slash_anime(slash_surf,slash_rect): #vágás animálása
+    screen.blit(slash_surf,slash_rect) 
+    #slash_surf = pygame.transform.rotozoom(slash_surf, 90, 1) #forgatás - sajnos nem működik
+    
 def display_score(): #pontok megjelenítése
-    score_surf=game_font.render('Score: '+str(score),True,FONT_COLOR) #hogyan
-    score_rect=score_surf.get_rect(topleft=(WIDTH-200,10)) #hol
-    screen.blit(score_surf,score_rect) #tadáám
+    score_surf=game_font.render('Score: '+str(score),True,FONT_COLOR) 
+    score_rect=score_surf.get_rect(center=(WIDTH/2,100)) 
+    screen.blit(score_surf,score_rect)
 
-pygame.init() #inicializálja magát a pygame
-screen=pygame.display.set_mode((WIDTH,HEIGHT)) #meghatározza az ablakot
-pygame.display.set_caption('Fruit Ninja') #főcím
-clock=pygame.time.Clock() #időzítő
+def display_time(time_left): #visszaszámlálás
+    time_left_surf=game_font.render("Time left: " + str(time_left), False, FONT_COLOR)
+    time_left_rect=time_left_surf.get_rect(center=(WIDTH//2,50))
+    screen.blit(time_left_surf,time_left_rect)
+
+def endgame(): #záróképernyő
+    global high_score, score, new_high_score
+    if score <= high_score:
+        score_surf=game_font.render('Your score: '+str(score),True,FONT_COLOR) 
+        score_rect=score_surf.get_rect(center=(WIDTH/2,HEIGHT/2-50)) 
+        screen.blit(score_surf,score_rect) 
+
+        high_score_surf=game_font.render('The high score: '+str(high_score),True,FONT_COLOR) 
+        high_score_rect=high_score_surf.get_rect(center=(WIDTH/2,HEIGHT/2+50)) 
+        screen.blit(high_score_surf,high_score_rect) 
+
+    if score>high_score:
+        score_surf=game_font.render('Your new high score: '+str(score),True,FONT_COLOR)
+        score_rect=score_surf.get_rect(center=(WIDTH/2,HEIGHT/2-50))
+        screen.blit(score_surf,score_rect)
+
+        new_high_score = score
+
+    enter_surf=game_font.render('Press ENTER to restart!',True,FONT_COLOR) 
+    enter_rect=enter_surf.get_rect(center=(WIDTH/2,HEIGHT-50)) 
+    screen.blit(enter_surf,enter_rect)
+
+def restart(): #újraindítás
+    global time_left, score, start_time, fruit_group, high_score, new_high_score
+    if score>high_score:
+        high_score=new_high_score
+    start_time=pygame.time.get_ticks()
+    time_left=61
+    score=0
+    ninja.sprite.kill()
+    ninja.add(Ninja())
+    fruit_group.empty()
+
+#PLATFORMOK
+platformSMALL_surf=pygame.image.load('img/platform_small.png').convert_alpha() 
+platformSMALL_rect=platformSMALL_surf.get_rect(midtop=(WIDTH/2,HEIGHT/4*2))
 
 platform_surf=pygame.image.load('img/platform.png').convert_alpha() #platform képe
-platform_rects=[platform_surf.get_rect(midtop=(WIDTH/2,HEIGHT-160)),
-                platform_surf.get_rect(midtop=(WIDTH+250,HEIGHT-200)),
-                platform_surf.get_rect(midtop=(-250,HEIGHT-230))] #platform helyei
+platform_rects=[#platform_surf.get_rect(midtop=(WIDTH/2,HEIGHT/4*2)), #felső #kivettem mert a SMALL jobb
+                platform_surf.get_rect(midtop=(WIDTH/2,HEIGHT-100)), #alsó
+                platform_surf.get_rect(midtop=(WIDTH+100,HEIGHT/3*2)), #jobb
+                platform_surf.get_rect(midtop=(-100,HEIGHT/3*2))] #bal
 
-ninja=pygame.sprite.GroupSingle() #példányosítom a ninját
+#Játékos
+ninja=pygame.sprite.GroupSingle() 
 ninja.add(Ninja())
 
-fruit_group=pygame.sprite.Group() #lehet sima group mert több objektumot tárol egyszerre
-fruit_timer=pygame.USEREVENT+1 #gyümölcsökhöz időzítő
-fruit_sec=1000 #időzítőhöz idő - 1 ms
-pygame.time.set_timer(fruit_timer,fruit_sec) #maga az időzítő elindul
+#Gyümölcsök
+fruit_group=pygame.sprite.Group() 
+fruit_timer=pygame.USEREVENT+1 #időzítő a sima gyümölcshöz
+fruit_sec=1000 #1sec
+pygame.time.set_timer(fruit_timer,fruit_sec)
 
-score=0 #számolja a pontjainkat
-game_font=pygame.font.SysFont('arial',30,bold=True) #betűtípus
+fruit_timer2=pygame.USEREVENT+2 #időzítő a sok gyümölcshöz
+fruit_sec2=17000 #17sec
+pygame.time.set_timer(fruit_timer2,fruit_sec2) 
 
-running=True #futás
-while running:
-    for event in pygame.event.get(): #események
-        if event.type==pygame.QUIT: #kilépés
-            running=False
-        if event.type==fruit_timer:
-           fruit_group.add(Fruit(random.choice(['pear','banana','strawberry']))) 
+#Pontok
+score=0 
+high_score=0 
+new_high_score=0
+time_left=61 #csak az induláshoz kell egy 0nál nagyobb érték
 
-
+def start():
     screen.fill(BG_COLOR) #háttérszín
-    screen.blit(background, (0, 0))  # Háttérkép kirajzolása a képernyőre
-    for platform_rect in platform_rects:
-        screen.blit(platform_surf,platform_rect) #platform megjeleítése
+    screen.blit(background, (0, 0))
 
-    ninja.draw(screen) #ninja megjelenítése
-    ninja.update() #frissítése
-    
-    #ELLENŐRZÉSHEZ KELLHET
-    #pygame.draw.rect(screen,'gray',ninja.sprite.rect,2) #kirajzolja a ninja rect vonalát
+    start_surf=game_font.render('FRUIT NINJA',True,FONT_COLOR) #hogyan
+    start_rect=start_surf.get_rect(center=(WIDTH/2,HEIGHT/2)) #hol
+    screen.blit(start_surf,start_rect) #tadáám
 
-    fruit_group.draw(screen) #gyümölcsök megjelenítése
-    fruit_group.update() #frissítése
+    start2_surf=game_font.render(f'wasd / arrows + space + ctrl',True,FONT_COLOR) #hogyan
+    start2_rect=start2_surf.get_rect(center=(WIDTH/2,HEIGHT/2+100)) #hol
+    screen.blit(start2_surf,start2_rect) #tadáám
 
-    if collision_sprite(): #ütközés
-        score+=1
-    display_score()
+    start3_surf=game_font.render('Press ENTER to start!',True,FONT_COLOR) #hogyan
+    start3_rect=start3_surf.get_rect(center=(WIDTH/2,HEIGHT-100)) #hol
+    screen.blit(start3_surf,start3_rect) #tadáám
 
-    pygame.display.update() #frissítés
+running=True #game loop
+go=False #kezdőképernyő
+while running:
+    keys = pygame.key.get_pressed()
+    for event in pygame.event.get(): #eseménykezelő
+        if event.type==pygame.QUIT: 
+            running=False
+        if event.type == pygame.KEYDOWN:  
+            if event.key == pygame.K_ESCAPE:  
+                running = False
+        if event.type==fruit_timer: #gyümölcs időzítő 1
+           fruit_group.add(Fruit(random.choice(['pear','banana','strawberry']))) 
+        if event.type==fruit_timer2: #gyümölcs időzítő 2
+            for i in range(7):
+                fruit_group.add(Fruit(random.choice(['pear','banana','strawberry']))) 
+
+    if not go: #kezdőképernyő
+        start()
+        if keys[pygame.K_RETURN] or keys[pygame.K_KP_ENTER]:
+            restart()
+            go=True
+
+    if go: #a játék
+        screen.fill(BG_COLOR) 
+        screen.blit(background, (0, 0)) #háttér
+        if time_left>0:
+            for platform_rect in platform_rects: #platformok
+                screen.blit(platform_surf,platform_rect) 
+            screen.blit(platformSMALL_surf,platformSMALL_rect)
+
+            ninja.draw(screen) #ninja 
+            ninja.update() 
+        
+        #ninja hitbox fejlesztéshez
+        #pygame.draw.rect(screen,'gray',ninja.sprite.rect,2) #kirajzolja a ninja rect vonalát
+
+            fruit_group.draw(screen) #gyümölcsök 
+            fruit_group.update()
+
+            if collision_sprite(): #ütközés ellenőrzés
+                score+=1
+
+            display_score() #pontok
+            display_time(time_left) #visszaszámlálás
+
+        time_left=int((start_time+GAME_TIME-pygame.time.get_ticks()))//1000 #visszaszámlálás - a max játékidőbl kivonja az eddig elteltet
+        if time_left<=0:
+            endgame() #záróképernyő
+            if keys[pygame.K_RETURN]:  # Billentyű lenyomás esemény # Ha az Enter-t nyomják meg
+                restart() #újraindítás
+
+    pygame.display.update() #képernyő frissítés
     clock.tick(FPS) #másodpercenként mennyi kép
 
-pygame.quit()
+pygame.quit() #bye bye
 
-#exportálás .exe fájlába
-#pyinstaller main.py --onefile --windowed
-
-#feladatok amiket meg kell csinálni
-#kezdő-záró képernyő
-#életerő, halál
-
+#Készítette: ecsitomi
+#HEJ 2025.
